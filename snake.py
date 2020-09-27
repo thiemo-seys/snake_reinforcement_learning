90ea51554e5ea5a8b8c6ab41737a117a0e9db16e
thiemo seys <thiemoseys123@gmail.com>
2020-09-27 12:39:15 +0200

    Beginning of test, expanded the get squares function

--- snake.py
+++ snake.py
@@ -1,14 +1,14 @@
 import random
-import math
-
 
 class Board():
     def __init__(self, width=10, height=10):
         self.width = width
         self.height = height
         self.board = [[Square(x=x, y=y) for x in range(self.width)] for y in range(self.height)]
+        #amount of iterations the board has gone through, also used to reduce the time to live of the food class
+        self.turn = 0
 
-    #TODO: this method needs to be split up in methods that each place a different object on the board
+    # TODO: this method needs to be split up in methods that each place a different object on the board
     def update_cell_object(self, x, y, new_object):
         self.board[x][y] = new_object
 
@@ -16,19 +16,48 @@ class Board():
         self.board[start_pos[0]][start_pos[1]] = Snake(start_pos)
 
     def placeFood(self, start_pos=None):
-        #no specific position was specified, so putting food in a random free place
+        # no specific position was specified, so putting food in a random free place
         if start_pos is None:
-           x,y = random.randint(0, self.width)
-           y = random.randint(0, self.height)
+            food_squares = self.get_food_squares()
+            x, y = random.randint(0, self.width), random.randint(0, self.height)
 
-    #check which squares contains a square object with value 1
-    def get_free_squares(self):
-        free_squares_list = []
-        for y, row in enumerate(self.board):
-            for x, square in enumerate(row):
-                if type(square) == 'Square' and :
+            # check if the random food square is not yet occupied
+            while (x, y) in food_squares:
+                x, y = random.randint(0, self.width), random.randint(0, self.height)
+        else:
+            x, y = start_pos
 
+        self.board[x][y] = Food(x, y)
 
+    def get_squares_containing_object(self, object_name):
+        # also add head and tail here
+        object_name_type_mapping = {'empty': None, 'snake': Snake, 'food': Food, 'head': Snake, 'tail': Snake}
+        squares_containing_object = []
+
+        for x, row in self.board:
+            for y, square in row:
+                #pycharm being a little bitch
+                #if isinstance(square.square_object, object_name_type_mapping[object_name]):
+                if isinstance(square.square_object, object_name_type_mapping.get(object_name)):
+                    squares_containing_object.append((x, y))
+
+        return squares_containing_object
+
+    # check which squares contains a square object with value 1
+    def get_empty_squares(self):
+        return self.get_squares_containing_object('empty')
+
+    def get_food_squares(self):
+        return self.get_squares_containing_object('food')
+
+    def get_snake_body_squares_pos(self):
+        self.get_squares_containing_object('snake')
+
+    def get_snake_tail_pos(self):
+        pass
+
+    def get_snake_head_pos(self):
+        pass
 
     def reset_board(self):
         self.board = [[Square(x=x, y=y) for x in range(self.width)] for y in range(self.height)]
@@ -37,25 +66,25 @@ class Board():
         for row in self.board:
             print(row)
 
-    #maybe override this method for pretty printing?
-    #but seems weird to print it pretty because return breaks the function
-    #def __str__(self):
+    # maybe override this method for pretty printing?
+    # but seems weird to print it pretty because return breaks the function
+    # def __str__(self):
     #    pass
 
 
-
 class Square():
-    def __init__(self, x, y, object = 0):
+    def __init__(self, x, y, square_object=None):
         self.x = x
         self.y = y
-        self.object = object
+        self.square_object = square_object
 
     def __str__(self):
         return str(self.object)
 
+
 #############################################################################################################################################
 class Snake():
-    def __init__(self, start_pos, dirx=1, diry=0, color=(255,0,0)):
+    def __init__(self, start_pos, dirx=1, diry=0):
         self.head = Cube(start_pos)
         self.body = []
         self.body.append(self.head)
@@ -64,22 +93,21 @@ class Snake():
         self.dirx = dirx
         self.diry = diry
         self.move = ''
-        self.color = color
 
     def add_cube(self):
         pass
 
     def change_direction(self, move):
-        possible_moves = {'up': [0,1], 'down': [0,-1], 'right': [1,0] , 'left': [-1,0] }
+        possible_moves = {'up': [0, 1], 'down': [0, -1], 'right': [1, 0], 'left': [-1, 0]}
         opposite_moves = {'up': 'down', 'down': 'up', 'right': 'left', 'left': 'right'}
 
         if move in possible_moves:
-            if  move != opposite_moves[self.move]:
+            if move != opposite_moves[self.move]:
                 self.dirx = possible_moves[move][0]
                 self.diry = possible_moves[move][1]
 
-                #possible do this instead, so we don't change references to variables
-                #self.turns[self.pos[:] = [self.dirx, self,diry]
+                # possible do this instead, so we don't change references to variables
+                # self.turns[self.pos[:] = [self.dirx, self,diry]
                 self.turns[self.pos] = [self.dirx, self.diry]
 
         for index, cube in enumerate(self.body):
@@ -92,19 +120,19 @@ class Snake():
 
 
 class Cube():
-    def __init__(self, start, dirx=1, diry=0, color=(255,255,255)):
+    def __init__(self, start, dirx=1, diry=0):
         self.pos = start
         self.dirx = dirx
         self.diry = diry
-        self.color = color
 
     def move(self, dirx, diry):
         self.dirx = dirx
         self.diry = diry
         self.pos = (self.pos[0] + self.dirx, self.pos[1] + self.diry)
 
+
 class Food():
-    def __init__(self, x, y, value=100, time_to_live = 100):
+    def __init__(self, x, y, value=100, time_to_live=100):
         self.x = x
         self.y = y
         self.value = value
@@ -112,5 +140,3 @@ class Food():
 
     def reduce_time_to_live(self):
         self.time_to_live += -1
-
-
---
+++ tests/test_snake.py
@@ -0,0 +1,5 @@
+import unittest
+
+from snake import *
+
+
