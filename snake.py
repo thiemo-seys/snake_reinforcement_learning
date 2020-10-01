e1390d22ab2ef26cb479dda136ceda469549561a
thiemo seys <thiemoseys123@gmail.com>
2020-10-01 19:12:25 +0200

    added more snake method and some tests

--- snake.py
+++ snake.py
@@ -1,16 +1,30 @@
 import random
 
+
 class Board():
     def __init__(self, width=10, height=10):
         self.width = width
         self.height = height
         self.board = [[Square(x=x, y=y) for x in range(self.width)] for y in range(self.height)]
-        #amount of iterations the board has gone through, also used to reduce the time to live of the food class
+        # amount of iterations the board has gone through, also used to reduce the time to live of the food class
+        self.snake = Snake((5, 5), dirx=1, diry=0)
         self.turn = 0
+
+
+    def move_snake(self):
+        self.snake.move_snake()
+
+    # sets the snake to the relevant squares on the board
+    def set_snake_to_board(self):
+        for snake_body_piece in self.snake.body:
+            print(f'setting snake body piece: {snake_body_piece}')
+            print(f'snake x= {snake_body_piece.pos[0]}')
+            x,y =snake_body_piece.pos[0], snake_body_piece.pos[1]
+            self.board[x][y].square_object = snake_body_piece
 
     # TODO: this method needs to be split up in methods that each place a different object on the board
     def update_cell_object(self, x, y, new_object):
-        self.board[x][y] = new_object
+        self.board[x][y].square_object = new_object
 
     def placeSnakeHead(self, start_pos):
         self.board[start_pos[0]][start_pos[1]] = Snake(start_pos)
@@ -19,28 +33,30 @@ class Board():
         # no specific position was specified, so putting food in a random free place
         if start_pos is None:
             food_squares = self.get_food_squares()
-            x, y = random.randint(0, self.width), random.randint(0, self.height)
+            x, y = random.randint(0, self.width - 1), random.randint(0, self.height - 1)
 
             # check if the random food square is not yet occupied
             while (x, y) in food_squares:
-                x, y = random.randint(0, self.width), random.randint(0, self.height)
+                x, y = random.randint(0, self.width - 1), random.randint(0, self.height - 1)
         else:
             x, y = start_pos
 
-        self.board[x][y] = Food(x, y)
+        self.board[x][y].square_object = Food(x, y)
 
     def get_squares_containing_object(self, object_name):
         # also add head and tail here
-        object_name_type_mapping = {'empty': None, 'snake': Snake, 'food': Food, 'head': Snake, 'tail': Snake}
+        object_name_type_mapping = {'empty': None, 'snake': Cube, 'food': Food, 'head': Snake, 'tail': Snake}
+
         squares_containing_object = []
 
-        for x, row in self.board:
-            for y, square in row:
-                #pycharm being a little bitch
-                #if isinstance(square.square_object, object_name_type_mapping[object_name]):
-                if isinstance(square.square_object, object_name_type_mapping.get(object_name)):
-                    squares_containing_object.append((x, y))
+        for x, row in enumerate(self.board):
+            for y, square in enumerate(row):
+                if object_name_type_mapping.get(object_name) is None:
+                    if square.square_object is None:
+                        squares_containing_object.append((x, y))
 
+                elif isinstance(square.square_object, object_name_type_mapping.get(object_name)):
+                    squares_containing_object.append((x, y))
         return squares_containing_object
 
     # check which squares contains a square object with value 1
@@ -50,8 +66,10 @@ class Board():
     def get_food_squares(self):
         return self.get_squares_containing_object('food')
 
-    def get_snake_body_squares_pos(self):
-        self.get_squares_containing_object('snake')
+    # TODO: check what is better
+    # this method can be changed to a method in the Snake class
+    def get_snake_body_squares(self):
+        return self.get_squares_containing_object('snake')
 
     def get_snake_tail_pos(self):
         pass
@@ -61,10 +79,6 @@ class Board():
 
     def reset_board(self):
         self.board = [[Square(x=x, y=y) for x in range(self.width)] for y in range(self.height)]
-
-    def display_board(self):
-        for row in self.board:
-            print(row)
 
     # maybe override this method for pretty printing?
     # but seems weird to print it pretty because return breaks the function
@@ -85,23 +99,30 @@ class Square():
 #############################################################################################################################################
 class Snake():
     def __init__(self, start_pos, dirx=1, diry=0):
-        self.head = Cube(start_pos)
+        self.head = Cube(start_pos, dirx=dirx, diry=diry)
         self.body = []
         self.body.append(self.head)
         self.turns = {}
         self.pos = start_pos
         self.dirx = dirx
         self.diry = diry
-        self.move = ''
 
-    def add_cube(self):
-        pass
+    # get pos and direction velocities from latest snake body part
+    def add_cube_to_tail(self):
+        # get info of last element in the snakes body
+        tail = self.body[-1]
+        dirx, diry = tail.dirx, tail.diry
+        x, y = tail.pos[0], tail.pos[1]
+        # TODO: debug this to see if we do not need to offset the cubes position in account of the snakes tails velocity
+        # add cube element after the last position in list
+        self.body.append(Cube(start=(x, y), dirx=dirx, diry=diry))
 
     def change_direction(self, move):
         possible_moves = {'up': [0, 1], 'down': [0, -1], 'right': [1, 0], 'left': [-1, 0]}
         opposite_moves = {'up': 'down', 'down': 'up', 'right': 'left', 'left': 'right'}
 
         if move in possible_moves:
+            # check if current move is not an impossible move
             if move != opposite_moves[self.move]:
                 self.dirx = possible_moves[move][0]
                 self.diry = possible_moves[move][1]
@@ -109,25 +130,33 @@ class Snake():
                 # possible do this instead, so we don't change references to variables
                 # self.turns[self.pos[:] = [self.dirx, self,diry]
                 self.turns[self.pos] = [self.dirx, self.diry]
-
-        for index, cube in enumerate(self.body):
-            cube_pos = cube.pos[:]
-            if cube_pos in self.turns:
-                turn = self.turns[cube_pos]
-                cube.move(turn[0], turn[1])
         else:
             raise ValueError(f'{move} is not a valid option, possible values: {[move for move in possible_moves]}')
 
+    def move_snake(self):
+        for cube in self.body:
+            cube_pos = cube.pos[:]
+            if cube_pos in self.turns:
+                turn = self.turns[cube_pos]
+                cube.set_direction(turn[0], turn[1])
+            #move the cube
+            cube.move()
 
+
+# a piece of snake Body
 class Cube():
-    def __init__(self, start, dirx=1, diry=0):
+    def __init__(self, start, dirx, diry):
         self.pos = start
         self.dirx = dirx
         self.diry = diry
 
-    def move(self, dirx, diry):
+    # sets the direction vectors of the cube
+    def set_direction(self, dirx, diry):
         self.dirx = dirx
         self.diry = diry
+
+    # moves the cube 1 turn further
+    def move(self):
         self.pos = (self.pos[0] + self.dirx, self.pos[1] + self.diry)
 
 
@@ -138,5 +167,16 @@ class Food():
         self.value = value
         self.time_to_live = time_to_live
 
-    def reduce_time_to_live(self):
-        self.time_to_live += -1
+    def reduce_time_to_live(self, amount=1):
+        self.time_to_live += -amount
+
+
+
+bord = Board()
+bord.set_snake_to_board()
+print(bord.board[5][5].square_object)
+print(bord.get_snake_body_squares())
+bord.move_snake()
+bord.set_snake_to_board()
+print(bord.board[5][5].square_object)
+print(bord.get_snake_body_squares())
--- tests/test_snake.py
+++ tests/test_snake.py
@@ -1,5 +1,18 @@
 import unittest
 
-from snake import *
+from snake import Snake
+
+#TODO: make something out of this class
+
+#empty board
+empty_board = []
+#board with free squares except in the corners
+
+#think about some other cases
+
+class TestBoard(unittest.TestCase):
+    def test_free_squares(self):
+        #test to check the free squares in a board
+        self.assertEqual()
 
 
